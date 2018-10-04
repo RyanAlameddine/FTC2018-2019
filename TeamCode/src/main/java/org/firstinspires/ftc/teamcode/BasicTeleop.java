d1546965d81ac69af7c41ec71afd97f900538072
Ryan Alameddine <rhalameddine@gmail.com>
2018-10-04 14:58:59 -0700

    merged with new setup. Added basic code for teleop and auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleop.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="BasicTeleop", group="Test")
+public class BasicTeleop extends LinearOpMode{
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Project0 robot = new Project0();
+
+        robot.init(hardwareMap);
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+            robot.rightMotor.setPower(gamepad1.right_stick_y);
+            robot.leftMotor .setPower(gamepad1.left_stick_y );
+        }
+
+        robot.rightMotor.setPower(0);
+        robot.leftMotor .setPower(0);
+    }
+}
